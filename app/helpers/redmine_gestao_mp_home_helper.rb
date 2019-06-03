c930306b15a103e19f2bb8a70f100095917fca25
Renato de Souza] <souzanato84@gmail.com>
2019-06-03 15:12:52 -0300

    Ajustes na tela inicial

--- app/helpers/redmine_gestao_mp_home_helper.rb
+++ app/helpers/redmine_gestao_mp_home_helper.rb
@@ -1,6 +1,6 @@
 module RedmineGestaoMpHomeHelper
 	def welcome_message
-		content = File.open(Rails.root + "#{Rails.root}/plugins/redmine_gestao_mp/README.md", 'r').read
-		GitHub::Markdown.render(content)
+		# content = File.open(Rails.root + "#{Rails.root}/plugins/redmine_gestao_mp/README.md", 'r').read
+		# GitHub::Markdown.render(content)
 	end
 end
